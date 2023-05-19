2fd8e62cfd1f48de864c0169ca6320cb27b16110
maliha20 <gulista.marium20@gmail.com>
2023-05-20 00:01:01 +0600

    my toy api done

--- index.js
+++ index.js
@@ -47,7 +47,11 @@ async function run() {
         return res.send(result)
       }
     })
-
+    
+    app.get('/mytoys/:email', async(req,res)=>{
+      const result = await toyCollection.find({sellersMail: req.params.email}).toArray()
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
