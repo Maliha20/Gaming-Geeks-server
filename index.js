a6541ebc76c76b2e875b9cde305040816bff3251
maliha20 <gulista.marium20@gmail.com>
2023-05-20 10:52:01 +0600

    delete a toy is done

--- index.js
+++ index.js
@@ -72,7 +72,14 @@ async function run() {
       const result = await toyCollection.find({sellersMail: req.params.email}).toArray()
       res.send(result)
     })
+     
 
+    app.delete('/mytoys/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.deleteOne(query)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
